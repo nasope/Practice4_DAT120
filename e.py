de9747e65ab94e6255a4f284b7b22b5bae87ed36
Nathaniel Solon Pedersen <nasope@gmail.com>
2023-09-14 14:31:34 +0200

    k.py

--- e.py
+++ e.py
@@ -14,3 +14,4 @@ for i in range(n):
 
 resultat = differanse(list)
 print(resultat)
+
--- k.py
+++ k.py
@@ -1,5 +1,22 @@
-from g import *
-from lister_for_del_1 import minsteKvadratersMetode
+def minsteKvadratersMetode(x: list, y: list):
+    n = len(x)
+
+    upper = 0
+    lower = 0
+
+    xAverage = sum(x) / len(x)
+    yAverage = sum(y) / len(y)
+
+    for i in range(n):
+        upper += (x[i]-xAverage)*(y[i]-yAverage)
+        lower += (x[i]-xAverage)**2
+
+    a = upper / lower
+    b = yAverage - a * xAverage
+
+    return (a,b)
+
+from lister_for_del_1 import *
 
 a,b = minsteKvadratersMetode(temperaturer,temperaturer_tidspunkter)
 
