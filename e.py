79ea780534a88b652421a4783128a414ad02c5e2
Nathaniel Solon Pedersen <55003068+nasope@users.noreply.github.com>
2023-09-14 14:10:01 +0200

    e.py & patch

---
+++ e.py
@@ -0,0 +1,16 @@
+# Øving 4, Oppgave e)
+def differanse(liste: list):
+    diff = []
+    for i in range(n-1):
+        diff.append(liste[i+1]-liste[i])
+    return diff
+
+n = int(input("Skriv inn antall tall du vil ha i listen din: "))
+
+liste = []
+for i in range(n):
+    tall = int(input())
+    liste.append(tall)
+
+resultat = differanse(list)
+print(resultat)
